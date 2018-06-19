c89c229e444493893198f574197b68c57206e620
Rohan Narain <rohannarain@Rohans-MacBook-Pro.local>
2018-06-19 10:10:49 -0700

    su18/hw/hw01.push

---
+++ materials/su18/hw/hw01/tests/q9_4.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 9_4',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> gradescope == 18969
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
